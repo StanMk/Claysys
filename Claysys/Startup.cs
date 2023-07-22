e2bd1878b9c9c89ab96da93a66930580f3ca5533
StanMk <37892546+StanMk@users.noreply.github.com>
2023-07-22 18:01:20 +0530

    Update Startup.cs

--- Claysys/Startup.cs
+++ Claysys/Startup.cs
@@ -33,7 +33,7 @@ namespace Claysys
              .AllowAnyMethod()
              .AllowCredentials()
              .SetIsOriginAllowed((host) => true)
-             .WithOrigins("https://polite-sea-06cc8e010.3.azurestaticapps.net/");
+             .WithOrigins("https://polite-sea-06cc8e010.3.azurestaticapps.net");
 
                 });
             });
